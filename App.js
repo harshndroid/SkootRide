9de649e0fb306d752dc3d4e8bcafa87ed1275073
harshndroid <52203103+harshndroid@users.noreply.github.com>
2021-07-17 16:22:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import TabNavigator from './src/Navigation/TabNavigator';
+
+export default function App() {
+  return (
+    <TabNavigator/>
+  );
+}
+
+const styles = StyleSheet.create({
+});
